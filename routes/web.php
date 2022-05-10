43c5aa5810ab04325f2dcd89538bbf7549093994
Dennis Wenrich <dennisfinley@gmail.com>
2022-05-10 15:03:39 -0400

    added routes for about and contact page

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,16 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/about', function () {
+    return view('Pages.about');
+});
+
+Route::get('/contact', function () {
+    return view('Pages.contact');
 });
