9b266a0f06b49ae3d4663e508effdb3d6b61fba9
Dennis Wenrich <dennisfinley@gmail.com>
2022-05-10 18:00:10 -0400

    added index method

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers;
 
 class PageController extends Controller
 {
+    public function index()
+    {
+        return view('Pages.index');
+    }
+
     public function about()
     {
         return view('Pages.about');
