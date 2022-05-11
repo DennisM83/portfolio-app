30aa06c07b0c648d93c7d198d15391865ec05f9e
Dennis Wenrich <dennisfinley@gmail.com>
2022-05-11 19:46:17 -0400

    update with breeze scaffold

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,24 @@
 <?php
 
-use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(PageController::class)->group(function () {
-    Route::get('/', 'index');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-    Route::get('/about', 'about');
-
-    Route::get('/contact', 'contact');
+Route::get('/', function () {
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
