e37eac6931c2d8cd452066b5770b904d55d742d7
Dennis Wenrich <dennisfinley@gmail.com>
2022-05-12 19:47:24 -0400

    added dynamic layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,5 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <title>{{ config('app.name', 'Laravel') }}</title>
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
+<x-guest-layout>
+    <x-navbar/>
     <h1 class="primary">Home</h1>
     <p>This is a sentence and I'm not sure what to write.</p>
-</body>
-</html>
+</x-guest-layout>
