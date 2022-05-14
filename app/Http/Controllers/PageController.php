2575678d08998b77cc5bcada3880d9cccabca7f5
Dennis Wenrich <dennisfinley@gmail.com>
2022-05-14 13:56:19 -0400

    update view to home page

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,7 +6,7 @@ class PageController extends Controller
 {
     public function index()
     {
-        return view('Pages.index');
+        return view('home');
     }
 
     public function about()
